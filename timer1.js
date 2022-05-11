35b6d5bddbe4e026b150c70bb3a4720143272157
Joshua Cheung <contact.cjoshua@gmail.com>
2022-05-10 17:59:16 -0700

    fix the timer

--- timer1.js
+++ timer1.js
@@ -2,11 +2,11 @@ const input = process.argv.slice(2);
 const arr = input.sort();
 
 let timer = 0;
-for (const beep of arr) {
-  timer = beep * 1000;
+for (let beep of arr) {
+  beep = beep * 1000;
+  timer = timer + beep;
   console.log(timer);
   setTimeout(() => {
     process.stdout.write("\x07");
-    console.log(".");
   }, timer);
 }
